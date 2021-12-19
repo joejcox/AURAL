f3dc20305d1ecbdefb20354098c8f10080c375b3
Joe Cox <joecox@live.co.uk>
2021-12-19 07:33:07 +0000

    docs (images): add logo files and home page mockup

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,8 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import App from "./App"
-import "./index.css"
-import { store } from "./app/store"
+import App from "app/App"
+import "index.css"
+import { store } from "app/store"
 import { Provider } from "react-redux"
 
 ReactDOM.render(

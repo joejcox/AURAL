f59ee51eff20fa6f0bd4d26da8adfd2934a39c13
Joe Cox <joecox@live.co.uk>
2021-12-19 07:38:41 +0000

    ci (tailwind.config.js): remove unused colour properties

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,9 +1,7 @@
+import screenshot from "assets/images/AURAL_HOME_UNAUTH_USER.png"
+
 const App = () => {
-  return (
-    <>
-      <h1>Aural</h1>
-    </>
-  )
+  return <img className="mx-auto" src={screenshot} alt="Aural Mockup" />
 }
 
 export default App

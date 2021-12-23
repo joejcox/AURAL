702c0a5a601ca6aeade106329a7310b6a62acd44
Joe Cox <joecox@live.co.uk>
2021-12-23 15:12:55 +0000

    styles: remove hover bg from mobile menu links

--- src/components/MobileMenu/MobileMenuLink.jsx
+++ src/components/MobileMenu/MobileMenuLink.jsx
@@ -4,7 +4,7 @@ const MobileMenuLink = ({ children, path }) => {
   return (
     <Link
       to={path}
-      className="px-6 py-3 w-full text-center lg:w-auto text-gray-50 hover:text-white text-lg inline-block uppercase font-extrabold font-primary hover:bg-main-400"
+      className="px-6 py-3 w-full text-center lg:w-auto text-gray-50 hover:text-white text-lg inline-block uppercase font-extrabold font-primary"
     >
       {children}
     </Link>

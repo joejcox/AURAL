e221797c6b1b4a3665dafd8965bb3c69a4963963
Joe Cox <joecox@live.co.uk>
2021-12-24 13:34:59 +0000

    refactor: move scrollTop to core components folder

--- src/core/Layout/index.jsx
+++ src/core/Layout/index.jsx
@@ -1,6 +1,6 @@
-import ScrollToTop from "components/ScrollToTop"
+import ScrollToTop from "core/ScrollToTop"
 import Header from "core/Layout/Header"
-import Footer from "./Footer"
+import Footer from "core/Layout/Footer"
 
 const Layout = ({ children }) => (
   <div className="flex flex-col min-h-screen bg-white font-primary">

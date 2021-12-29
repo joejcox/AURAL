aa7b5c33eff741809f81244545ec26afbf2ef4a3
Joe Cox <joecox@live.co.uk>
2021-12-29 14:17:45 +0000

    Add Helmet title to shop and sign in pages

--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -1,11 +1,15 @@
 import ProductGridItem from "components/Products/ProductGridItem"
 import ProductGridContainer from "components/Products/ProductGridItem/ProductGridContainer"
 import useProducts from "hooks/useProducts"
+import { Helmet } from "react-helmet-async"
 
 const Products = () => {
   const { products } = useProducts()
   return (
     <ProductGridContainer>
+      <Helmet>
+        <title>Shop | Aural Records</title>
+      </Helmet>
       {products.map((product) => (
         <ProductGridItem product={product} key={product.id} />
       ))}
--- src/pages/public/SignIn/index.jsx
+++ src/pages/public/SignIn/index.jsx
@@ -1,6 +1,7 @@
 import SignInForm from "components/SignInForm"
 import { Navigate } from "react-router-dom"
 import useAuth from "hooks/useAuth"
+import { Helmet } from "react-helmet-async"
 
 const SignIn = () => {
   const { user } = useAuth()
@@ -9,6 +10,9 @@ const SignIn = () => {
     <Navigate to="/" />
   ) : (
     <SignInForm>
+      <Helmet>
+        <title>Sign In | Aural Records</title>
+      </Helmet>
       <h1 className="text-3xl mb-6 font-secondary">Sign In</h1>
     </SignInForm>
   )

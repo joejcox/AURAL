c64597edcd0a9dc82c91507e0323da7fbac1bbc7
Joe Cox <joecox@live.co.uk>
2021-12-30 15:24:21 +0000

    persist cart store

--- src/app/store.js
+++ src/app/store.js
@@ -1,12 +1,24 @@
 import { configureStore } from "@reduxjs/toolkit"
+
+import { persistStore, persistReducer } from "redux-persist"
+import storage from "redux-persist/lib/storage" // defaults to localStorage for web
+
 import productsReducer from "features/products/productsSlice"
 import userReducer from "features/user/userSlice"
 import cartReducer from "features/cart/cartSlice"
+
+const cartPersistConfig = {
+  key: "cart",
+  storage: storage,
+  blacklist: ["products", "user"],
+}
 
 export const store = configureStore({
   reducer: {
     products: productsReducer,
     user: userReducer,
-    cart: cartReducer,
+    cart: persistReducer(cartPersistConfig, cartReducer),
   },
 })
+
+export const persistor = persistStore(store)
--- src/index.jsx
+++ src/index.jsx
@@ -3,16 +3,19 @@ import ReactDOM from "react-dom"
 import App from "app/App"
 import "index.css"
 import { BrowserRouter } from "react-router-dom"
-import { store } from "app/store"
+import { store, persistor } from "app/store"
 import { Provider } from "react-redux"
 import { HelmetProvider } from "react-helmet-async"
+import { PersistGate } from "redux-persist/integration/react"
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <HelmetProvider>
         <Provider store={store}>
-          <App />
+          <PersistGate loading={null} persistor={persistor}>
+            <App />
+          </PersistGate>
         </Provider>
       </HelmetProvider>
     </BrowserRouter>

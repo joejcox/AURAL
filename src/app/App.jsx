c4249a265eaab67c4fb210f394d8814a2ecd7ad8
Joe Cox <joecox@live.co.uk>
2021-12-31 20:08:02 +0000

    add productSlice error

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -12,8 +12,10 @@ import { setError, setUser } from "features/user/userSlice"
 import SignIn from "pages/public/SignIn"
 import SignUp from "pages/public/SignUp"
 import Cart from "components/Cart"
+import useProducts from "hooks/useProducts"
 
 const App = () => {
+  const { error, status } = useProducts()
   const dispatch = useDispatch()
 
   useEffect(() => {
@@ -42,9 +44,27 @@ const App = () => {
     dispatch(getProductsFromFirestore())
   }, [dispatch])
 
+  if (error)
+    return (
+      <div className="py-16 bg-gray-100 text-3cl text-center">501 Error</div>
+    )
+
+  if (status === "loading")
+    return (
+      <div className="py-16 bg-gray-100 text-3cl text-center">Loading...</div>
+    )
+
   return (
     <Layout>
       <Routes>
+        <Route
+          path="*"
+          element={
+            <div className="py-16 text-3xl text-center bg-gray-100">
+              404 Page Not Found
+            </div>
+          }
+        />
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Shop />} />
         <Route path="/products/:productId" element={<SingleProduct />} />
--- src/hooks/useProducts.js
+++ src/hooks/useProducts.js
@@ -2,9 +2,9 @@ import { useSelector } from "react-redux"
 import { selectProducts } from "features/products/productsSlice"
 
 const useProducts = () => {
-  const { products, featured, status } = useSelector(selectProducts)
+  const { products, featured, status, error } = useSelector(selectProducts)
 
-  return { products, featured, status }
+  return { products, featured, status, error }
 }
 
 export default useProducts
